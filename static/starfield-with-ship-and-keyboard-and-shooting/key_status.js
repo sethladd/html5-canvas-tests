f4534f6832556a654a3f84bf839baab2c14ec07b
Seth Ladd <sethladd@gmail.com>
2011-01-31 21:00:04 -0800

    now with shooting

---
+++ static/starfield-with-ship-and-keyboard-and-shooting/key_status.js
@@ -0,0 +1,14 @@
+window.keydown = {};
+  
+  function keyName(event) {
+    return jQuery.hotkeys.specialKeys[event.which] ||
+      String.fromCharCode(event.which).toLowerCase();
+  }
+  
+  $(document).bind("keydown", function(event) {
+    keydown[keyName(event)] = true;
+  });
+  
+  $(document).bind("keyup", function(event) {
+    keydown[keyName(event)] = false;
+  });
